9627662a0c320e4e25c509dcc8192de30f6741fd
j2forem <25668799+j2forem@users.noreply.github.com>
2024-10-17 15:22:48 -0700

    Delete Data directory
    Don't need it on github

--- Data/config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-// Path to your Firebase service account file
-const serviceAccountPath = 'C:\\Users\\User\\Documents\\Take Two\\weekendweebz-firebase-adminsdk-3ip3p-8bf8899cd9.json';
-
-module.exports = {
-  serviceAccountPath
-};
